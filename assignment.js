2175bfc3dce00eb121bc3347c0059b6c43a1821c
HimanshuAmola <92074296+HimanshuAmola@users.noreply.github.com>
2021-12-09 21:09:44 +0530

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -42,3 +42,23 @@ function square(ar1){
     };
     square();
 
+
+//Integer
+
+function div(integer){
+  const y = 5;
+  if(integer%5==0){
+    return "divisible by 5";
+  }  
+  else{
+      return "it is not divisible by 5";
+  }
+
+  }
+console.log(div(15));
+console.log(div(25));
+console.log(div(10));
+console.log(div(16));
+console.log(div(5));
+
+

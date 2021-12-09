0a557c7a5ac9b498f2ca2870542619159bc89cb8
Shreyansh adarsh <70903823+kumaradarsh62@users.noreply.github.com>
2021-12-09 20:45:07 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+
+let variableA="sss";
+var variableB="bbb";
+const variableC="ccc";
+console.log(variableA);
+console.log(variableB);
+console.log(variableC);
+let variableD="update";
+console.log(variableD);
+
+var variableB="update";
+console.log(variableB);
+
+console.log("susbribe now")
+
+test();
+function test(){
+    console.log("this is test function");
+}
